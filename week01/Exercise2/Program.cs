050e9d793d0174a9d505efcf8ada0666cafb6620
Pavel Shvets <38735112+pavelwebdeveloper@users.noreply.github.com>
2025-04-24 21:13:51 -0600

    added code for Exercise 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -5,5 +5,75 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
+
+        Console.Write("What is your grade percentage? ");
+
+        string stringGradePercentage = Console.ReadLine();
+
+        int numberGradePercentage = int.Parse(stringGradePercentage);
+
+        string letter;
+
+        string sign = "";
+
+        if (numberGradePercentage >= 90)
+        {
+            letter = "A";
+            if(numberGradePercentage % 10 < 3)
+            {
+                sign = "-";
+            }
+        } 
+        else if (numberGradePercentage >= 80)
+        {
+            letter = "B";
+            if(numberGradePercentage % 10 >= 7)
+            {
+                sign = "+";
+            }
+            else if (numberGradePercentage % 10 < 3)
+            {
+                sign = "-";
+            }
+        }
+        else if (numberGradePercentage >= 70)
+        {
+            letter = "C";
+            if(numberGradePercentage % 10 >= 7)
+            {
+                sign = "+";
+            }
+            else if (numberGradePercentage % 10 < 3)
+            {
+                sign = "-";
+            }
+        }
+        else if (numberGradePercentage >= 60)
+        {
+            letter = "D";
+            if(numberGradePercentage % 10 >= 7)
+            {
+                sign = "+";
+            }
+            else if (numberGradePercentage % 10 < 3)
+            {
+                sign = "-";
+            }
+        }
+        else
+        {
+            letter ="F";
+        }
+
+        Console.WriteLine($"Your grade is {letter}{sign}");
+
+        if(numberGradePercentage >= 70)
+        {
+            Console.WriteLine("Congratulations! You have passed the class!");
+        }
+        else
+        {
+           Console.WriteLine("Sorry but you got a failing grade. Please, try to take the class again."); 
+        }
     }
 }
