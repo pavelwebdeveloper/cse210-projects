187b5424d0227580e0a93bedf4c356ffe7880473
Pavel Shvets <38735112+pavelwebdeveloper@users.noreply.github.com>
2025-04-25 10:13:47 -0600

    added code for Exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,5 +5,34 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+        Console.Write("What is the magic number? ");
+
+        string stringMagicNumber = Console.ReadLine();
+
+        int intMagicNumberFromUser = int.Parse(stringMagicNumber);
+
+        string stringUserGuess;
+        int intUserGuess;
+
+        do
+        {
+            Console.Write("What is your guess? ");
+
+            stringUserGuess = Console.ReadLine();
+
+            intUserGuess = int.Parse(stringUserGuess);
+
+            if(intMagicNumberFromUser > intUserGuess)
+            {
+                Console.WriteLine("Higher");
+            } 
+            else if(intMagicNumberFromUser < intUserGuess)
+            {
+                Console.WriteLine("Lower");
+            }
+        }while(intMagicNumberFromUser != intUserGuess);
+
+        Console.WriteLine("You guessed it!");
     }
 }
