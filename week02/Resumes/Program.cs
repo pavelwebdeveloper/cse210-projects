f2ef33df7cbbd6918629e0b14c19bcbd67a6b098
Pavel Shvets <38735112+pavelwebdeveloper@users.noreply.github.com>
2025-04-30 00:12:24 -0600

    created classes Job and Resume. Added code to create jobs and to display the information about jobs through Resume object

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -5,5 +5,25 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Resumes Project.");
+
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "EPAM";
+        job1._startYear = 2015;
+        job1._endYear = 2020;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Developer";
+        job2._company = "Luxsoft";
+        job2._startYear = 2020;
+        job2._endYear = 2023;
+
+
+        Resume resume = new Resume();
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+
+        resume.Display();
+        
     }
 }
