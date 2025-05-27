62926829deb1f86782beccd0f8c952c6c48d488c
Pavel Shvets <38735112+pavelwebdeveloper@users.noreply.github.com>
2025-05-27 06:05:19 -0600

    added Square, Rectangle and Circle classes

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -5,5 +5,19 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Shapes Project.");
-    }
+
+        List<Shape> _shapes = new List<Shape>();
+
+        _shapes.Add(new Square("green", 2));
+        _shapes.Add(new Rectangle("red", 3, 4));
+        _shapes.Add(new Circle("blue", 5));
+
+        foreach (Shape shape in _shapes)
+        {
+            Console.WriteLine(shape.GetColor());
+            Console.WriteLine(shape.GetArea());
+            Console.WriteLine();
+        }
+        
+    }    
 }
